1c2cccdb7009e26b09a4847d5c1093f17e2b30f9
Huy Ngô <huyngo@Huys-MacBook-Air.local>
2025-10-28 13:20:24 +0700

    ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,28 @@ import taskRouters from './src/routes/taskRouters.js';
 import { connectDB } from './src/config/db.js';
 import { configDotenv } from 'dotenv';
 import cors from 'cors';
+import path from 'path';
+
 configDotenv()
 const Port = process.env.PORT || 5001;
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+
+if(process.env.NODE_ENV !== 'production'){
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
+
 app.use("/api/tasks", taskRouters);
+
+if(process.env.NODE_ENV === 'production'){
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
+}
+
 connectDB().then(() => {
   console.log("Connected to the database");
   app.listen(Port, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+const baseURL = import.meta.env.MODE === 'development' ? 'http://localhost:5001/api' : '/api';
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: baseURL,
 
 });
 export default api;
